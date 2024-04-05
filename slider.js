7a50629674b051dd0d7c6e830a5713314f03291a
Walter Koren <walter.koren@hotmail.nl>
2024-04-05 14:04:38 +0200

    individueel product
    layout en slidebar gemaakt

---
+++ slider.js
@@ -0,0 +1,22 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const prevButton = document.querySelector('.slider-button.prev');
+const nextButton = document.querySelector('.slider-button.next');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.style.opacity = i === index ? '1' : '0';
+  });
+}
+
+prevButton.addEventListener('click', () => {
+  currentSlide = currentSlide > 0 ? currentSlide - 1 : slides.length - 1;
+  showSlide(currentSlide);
+});
+
+nextButton.addEventListener('click', () => {
+  currentSlide = currentSlide < slides.length - 1 ? currentSlide + 1 : 0;
+  showSlide(currentSlide);
+});
+
+showSlide(currentSlide);
