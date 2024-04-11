7f318278b6ed953aef8e8615288e29e30ba7c00a
Nick Colijn <nickcolijn2006@gmail.com>
2024-04-11 13:02:59 +0200

    product pagina nick

--- slider.js
+++ slider.js
@@ -1,20 +1,20 @@
 let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
-const prevButton = document.querySelector('.slider-button.prev');
-const nextButton = document.querySelector('.slider-button.next');
+const slides = document.querySelectorAll(".slide");
+const prevButton = document.querySelector(".slider-button.prev");
+const nextButton = document.querySelector(".slider-button.next");
 
 function showSlide(index) {
   slides.forEach((slide, i) => {
-    slide.style.opacity = i === index ? '1' : '0';
+    slide.style.opacity = i === index ? "1" : "0";
   });
 }
 
-prevButton.addEventListener('click', () => {
+prevButton.addEventListener("click", () => {
   currentSlide = currentSlide > 0 ? currentSlide - 1 : slides.length - 1;
   showSlide(currentSlide);
 });
 
-nextButton.addEventListener('click', () => {
+nextButton.addEventListener("click", () => {
   currentSlide = currentSlide < slides.length - 1 ? currentSlide + 1 : 0;
   showSlide(currentSlide);
 });
